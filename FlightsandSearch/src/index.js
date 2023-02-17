e6d69a7e83e836fb2bbf2b90461c58e6d8f3bbef
Nishant <Nishantshelar56@gmail,com>
2023-02-17 11:58:49 +0530

    setup basic env

---
+++ FlightsandSearch/src/index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+
+
+const {PORT}=require('./config/serverConfig.js')
+
+const setupandStartServer=async ()=>{
+
+    const app=express();
+    app.listen(PORT,()=>{
+        console.log(`server is running at port ${PORT}`)
+    })
+}
+
+setupandStartServer();
+
