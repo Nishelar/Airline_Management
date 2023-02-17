447fc27921728b17651f9656511fbb8c25d24b63
Nishant <Nishantshelar56@gmail,com>
2023-02-17 12:38:19 +0530

    Initialize the sequelize

--- FlightsandSearch/src/index.js
+++ FlightsandSearch/src/index.js
@@ -1,11 +1,13 @@
 const express=require("express");
-
+const bodyparser=require("body-parser")
 
 const {PORT}=require('./config/serverConfig.js')
 
 const setupandStartServer=async ()=>{
 
     const app=express();
+    app.use(bodyparser.json())
+    app.use(bodyparser.urlencoded({extended:true}))
     app.listen(PORT,()=>{
         console.log(`server is running at port ${PORT}`)
     })
