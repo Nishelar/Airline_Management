8ffd33a1bc62d3799b48d970920fff665621c94f
Nishant <Nishantshelar56@gmail,com>
2023-02-17 17:41:58 +0530

    Created Repository to add and Delete Airplanes

--- FlightsandSearch/src/index.js
+++ FlightsandSearch/src/index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const bodyparser=require("body-parser")
-
+//const CityRepository=require("./repository/city-repository")
 const {PORT}=require('./config/serverConfig.js')
 
 const setupandStartServer=async ()=>{
@@ -10,6 +10,8 @@ const setupandStartServer=async ()=>{
     app.use(bodyparser.urlencoded({extended:true}))
     app.listen(PORT,()=>{
         console.log(`server is running at port ${PORT}`)
+        //const repo=new CityRepository();
+        //repo.createCity({name:"Mumbai"});
     })
 }
 
