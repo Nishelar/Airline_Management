9f86e12f7a501ccd4b25fa9477ada4b762ad36ad
Nishant <Nishantshelar56@gmail,com>
2023-03-02 14:33:02 +0530

    successfully created repository layer

--- FlightsandSearch/src/index.js
+++ FlightsandSearch/src/index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const bodyparser=require("body-parser")
-//const CityRepository=require("./repository/city-repository")
+const CityRepository=require("./repository/city-repository")
 const {PORT}=require('./config/serverConfig.js')
 
 const setupandStartServer=async ()=>{
@@ -11,7 +11,7 @@ const setupandStartServer=async ()=>{
     app.listen(PORT,()=>{
         console.log(`server is running at port ${PORT}`)
         //const repo=new CityRepository();
-        //repo.createCity({name:"Mumbai"});
+        //repo.createCity({name:"Chennai"});
     })
 }
 
